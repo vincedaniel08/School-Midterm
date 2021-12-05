ac741c9c45019eaf8780f03f38107f48db1132b8
vincedaniel08 <vincedaniel20@gmail.com>
2021-12-05 15:58:19 +0800

    firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,13 +8,12 @@ import 'firebase/compat/firestore';
 //import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBxFCFEF7XICubpyCvpnfCKGZKD81FJeuA",
-  authDomain: "midterm-4th-year-29426.firebaseapp.com",
-  projectId: "midterm-4th-year-29426",
-  storageBucket: "midterm-4th-year-29426.appspot.com",
-  messagingSenderId: "343266854129",
-  appId: "1:343266854129:web:6e7b7d3767d3f38d0a9550",
-  measurementId: "G-8LSER1GD13"
+  apiKey: "AIzaSyCFHwhvdSWeh3UG-ur1lMBRi2A9irp3E1M",
+  authDomain: "midterm-4th-year.firebaseapp.com",
+  projectId: "midterm-4th-year",
+  storageBucket: "midterm-4th-year.appspot.com",
+  messagingSenderId: "747362732932",
+  appId: "1:747362732932:web:20ecc0842d2ff0582c6bd1"
   };
   const app = initializeApp(firebaseConfig);
 
