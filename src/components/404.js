9ec8c49785c4fdd62e316a786484a7557bc0c91f
vincedaniel08 <vincedaniel20@gmail.com>
2021-12-11 13:31:43 +0800

    helmet

--- src/components/404.js
+++ src/components/404.js
@@ -2,13 +2,21 @@ import { Box, Typography } from '@mui/material'
 import React from 'react'
 import Header from './Header'
 import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
-
+import { Helmet } from "react-helmet";
 
 
 
 export default function NotFound() {
     return (
         <Box>
+               <Helmet>
+                <title>Not Found</title>
+                <meta
+                  name="Page not found"
+                  content="Vince Daniel De Leon, Bryce Ganotice, Harold Dimson, Ronnie Bagonas, Nico Mapoy"
+                />
+                
+            </Helmet>
             <Header/>
             <Box sx={{display:"flex",flexDirection:"column", alignItems:"center",justifyContent:"center", alignContent:"center", mt:10}}>
             <SentimentVeryDissatisfiedIcon sx={{color:"white", fontSize:200}}/>
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,19 @@
 import { Box, Typography } from '@mui/material'
 import React from 'react'
 import Header from './Header'
+import { Helmet } from "react-helmet";
+
 export default function Blog() {
     return (
         <Box>
+               <Helmet>
+                <title>Blog</title>
+                <meta
+                  name="Blog Students"
+                  content="Vince Daniel De Leon, Bryce Ganotice, Harold Dimson, Ronnie Bagonas, Nico Mapoy"
+                />
+                
+            </Helmet>
             <Header/>
             <Typography variant="h1" sx={{color:"white", textAlign:"center", mt:10}}>Blog</Typography>
         </Box>
